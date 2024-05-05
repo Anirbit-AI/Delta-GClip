21e8d2e71cf5d35d033faec9b23b01f7ee63eabd
PROCHETA SEN <senprocheta@gmail.com>
2024-05-05 21:00:51 +0100

    Update Delta-GClip.py

--- Delta-GClip.py
+++ Delta-GClip.py
@@ -1,10 +1,10 @@
 from torch.optim.optimizer import Optimizer, required
 import copy
 
-class Delta-GClip(Optimizer):
+class deltaGClip(Optimizer):
     def __init__(self, params, lr=0.1, delta=1e-03, gamma=0.25):
         defaults = dict(lr=lr, delta=delta, gamma=gamma)
-        super(deltaGClipOptimizer, self).__init__(params, defaults)
+        super(deltaGClip, self).__init__(params, defaults)
 
     @torch.no_grad()
     def step(self, closure=None):
@@ -22,9 +22,4 @@ class Delta-GClip(Optimizer):
                     g_norm = min(1, max(delta, gamma / norm))
                     scaled_grad = g_norm * grad
                     p.data -= lr * scaled_grad
-   def step(self,closure=None):
-	norm_grad_f = torch.nn.utils.clip_grad_norm_(net.parameters(), max_norm=float("inf")).item()
-        h = min(eta, eta * max(delta, gamma / norm_grad_f) )
-        for g in optimizer.param_groups:
-        	g["lr"] = h
   
