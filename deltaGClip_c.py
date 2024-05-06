ab1a9b371cd2dbc2920c5e1bac67ea5077d285af
Anirbit <106439647+Anirbit-AI@users.noreply.github.com>
2024-05-06 16:27:53 +0100

    Update and rename deltaGClip-c.py to deltaGClip_c.py

--- deltaGClip_c.py
+++ deltaGClip_c.py
@@ -2,10 +2,10 @@ from torch.optim.optimizer import Optimizer, required
 import copy
 import torch
 
-class deltaGClip-c(Optimizer):
+class deltaGClip_c(Optimizer):
     def __init__(self, params, lr=0.1, delta=1e-03, gamma=0.25):
         defaults = dict(lr=lr, delta=delta, gamma=gamma)
-        super(deltaGClip-c, self).__init__(params, defaults)
+        super(deltaGClip_c, self).__init__(params, defaults)
 
     @torch.no_grad()
     def step(self, closure=None):
